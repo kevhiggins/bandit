0deb8de51f931d0e65b8bfa019938cbdc773b76e
Kevin Higgins <kev.higgins@gmail.com>
2017-03-16 23:47:38 -0500

    Enh: Add the ability to spawn bandits with a UI button.

---
+++ Assets/Scripts/App/HudInputManager.cs
@@ -0,0 +1,19 @@
+﻿using App.Unit;
+using UnityEngine;
+
+namespace App
+{
+    class HudInputManager : MonoBehaviour
+    {
+        public GameObject banditStartWaypoint = null;
+        public GameObject banditPrefab = null;
+
+        public void PurchaseBandit()
+        {
+            var banditInstance = Instantiate(banditPrefab, banditStartWaypoint.transform.position, Quaternion.identity);
+            var bandit = banditInstance.GetComponent<Bandit>();
+            bandit.targetWaypoint = banditStartWaypoint;
+            bandit.Init();
+        }
+    }
+}
