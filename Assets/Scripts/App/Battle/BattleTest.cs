46217aa98d3b29023b7b65c7761ba32e92f0d191
Kevin Higgins <kev.higgins@gmail.com>
2017-03-27 22:19:38 -0500

    Enh: Start adding bandit pokemon.

---
+++ Assets/Scripts/App/Battle/BattleTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.Battle
+{
+    class BattleTest : MonoBehaviour
+    {
+        public List<GameObject> travelers;
+        public List<GameObject> bandits;
+
+        public void Awake()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/App/Battle/Combatant.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace App.Battle
+{
+    public class Combatant : MonoBehaviour
+    {
+        public int hp = 6;
+        public int attack = 2;
+        public int threat = 1;
+    }
+}
