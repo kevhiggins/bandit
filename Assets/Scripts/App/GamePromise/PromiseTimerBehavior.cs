779ce66da0917549842be7f69402659743104528
Kevin Higgins <kev.higgins@gmail.com>
2017-04-23 22:56:35 -0500

    Enh: Add helper classes for integrating Promise library with Unity

---
+++ Assets/Scripts/App/GamePromise/PromiseTimerBehavior.cs
@@ -0,0 +1,25 @@
+﻿using RSG;
+using UnityEngine;
+
+namespace App.GamePromise
+{
+    class PromiseTimerBehavior : MonoBehaviour
+    {
+        private static bool isExceptionHandlerRegistered = false;
+
+        void Awake()
+        {
+            if (!isExceptionHandlerRegistered)
+            {
+                Promise.UnhandledException += (sender, args) => { Debug.Log(args.Exception); };
+                isExceptionHandlerRegistered = true;
+            }
+            
+        }
+
+        void Update()
+        {
+            PromiseTimerHelper.Update(Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/App/GamePromise/PromiseTimerHelper.cs
@@ -0,0 +1,19 @@
+﻿using RSG;
+
+namespace App.GamePromise
+{
+    class PromiseTimerHelper
+    {
+        private static PromiseTimer instance;
+
+        public static PromiseTimer Instance {
+            get { return instance ?? (instance = new PromiseTimer()); }
+            set { instance = value; }
+        }
+
+        public static void Update(float deltaTime)
+        {
+            Instance.Update(deltaTime);
+        }
+    }
+}
