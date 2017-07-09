d1b6fae2bf6d6fbc5cd2e7d08f077f94ed4075d1
Kevin Somers-Higgins <kev.higgins@gmail.com>
2017-07-08 22:49:22 -0500

    Add StateTriggers script for triggering events on state enter and exit

---
+++ Assets/Scripts/App/UI/StateMachine/StateTriggers.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace App.UI.StateMachine
+{
+    class StateTriggers : StateMachineBehaviour
+    {
+        public UnityEvent onStateEnter = new UnityEvent();
+        public UnityEvent onStateExit = new UnityEvent();
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            onStateEnter.Invoke();
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            onStateExit.Invoke();
+        }
+    }
+}
