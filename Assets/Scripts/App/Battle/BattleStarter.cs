3a9b7f2a945599a25625e65cb1d641002f484703
Kevin Somers-Higgins <kev.higgins@gmail.com>
2017-07-08 22:50:45 -0500

    Add BattleStarter script to test starting the battle overlay from the main scene

---
+++ Assets/Scripts/App/Battle/BattleStarter.cs
@@ -0,0 +1,20 @@
+﻿using App.GamePromise;
+using RSG;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace App.Battle
+{
+    class BattleStarter : MonoBehaviour
+    {
+        public void StartBattle()
+        {
+            GameManager.instance.TogglePause();
+
+            var loadSceneInstruction = SceneManager.LoadSceneAsync("Scenes/Banditmon", LoadSceneMode.Additive);
+            YieldPromise.Generate(loadSceneInstruction);
+        }
+    }
+}
