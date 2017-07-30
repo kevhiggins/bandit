343dcf6f5c2e3c8ef9834e0e1fc8d4a0bbf6f8db
Kevin Somers-Higgins <kev.higgins@gmail.com>
2017-07-30 17:42:32 -0500

    Simplify Deck/Card DI binding

--- Assets/Scripts/App/Installer/GameSettingsInstaller.cs
+++ Assets/Scripts/App/Installer/GameSettingsInstaller.cs
@@ -10,24 +10,11 @@ namespace App.Installer
     {
         public Deck eventDeck;
 
-        [NonSerialized]
-        private bool isDeckInjected = false;
-
         public override void InstallBindings()
         {
-            // TODO Cards should be CardSetting objects containing a quantity.
-            // TODO We should generate the correct number of cards from those settings.
-            // TODO figure out if there is a way to match the card setting to the appropriate service, so we only have to inject the card event resolvers instead of each card.
-
-            // Bind Deck, and inject into the deck and all of its cards on the first run.
+            // Bind Deck, and inject into the deck and all of its cards.
             Container.Bind<Deck>().FromMethod(ctx =>
             {
-                Debug.Log(isDeckInjected);
-                if (isDeckInjected)
-                    return eventDeck;
-
-                isDeckInjected = true;
-
                 Container.Inject(eventDeck);
 
                 foreach (var card in eventDeck.cards)
@@ -35,10 +22,8 @@ namespace App.Installer
                     Container.Inject(card);
                 }
 
-                Container.Bind<Deck>().FromInstance(eventDeck);
-
                 return eventDeck;
-            });
+            }).AsSingle();
         }
     }
 }
